5996e71453b67902960c7a01c407a9424b7d673b
Bardak <iiidaniiliii6183@gmail.com>
2024-12-31 10:00:33 +0300

    Коммит

--- SystemControleProject/wsgi.py
+++ SystemControleProject/wsgi.py
@@ -1,16 +1,18 @@
-"""
-WSGI config for SystemControleProject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# Укажите путь к проекту
+project_path = '/home/Bardak/SystemControleProject'
+if project_path not in sys.path:
+    sys.path.append(project_path)
+
+# Укажите путь к виртуальному окружению
+activate_env = '/home/Bardak/SystemControleProject/venv/bin/activate'
+exec(open(activate_env).read(), {'__file__': activate_env})
+
+# Укажите настройки Django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'SystemControleProject.settings'
+
+# Загрузка WSGI приложения
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SystemControleProject.settings')
-
 application = get_wsgi_application()
