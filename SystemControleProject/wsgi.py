0659eb1a75f0ff3bdfb9b08cbbe0b65d48267a2d
Bardak <iiidaniiliii6183@gmail.com>
2024-12-31 10:19:12 +0300

    Коммит

--- SystemControleProject/wsgi.py
+++ SystemControleProject/wsgi.py
@@ -7,7 +7,7 @@ if project_path not in sys.path:
     sys.path.append(project_path)
 
 # Укажите путь к виртуальному окружению
-activate_env = '/home/Bardak/SystemControleProject/venv/bin/activate'
+activate_env = '/home/Bardak/IT/curs/venv/bin/activate'
 exec(open(activate_env).read(), {'__file__': activate_env})
 
 # Укажите настройки Django
