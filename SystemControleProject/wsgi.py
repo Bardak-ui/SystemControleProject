17497c0e31ee6ee3d1de96b2d457bab94d1d1783
Bardak <iiidaniiliii6183@gmail.com>
2024-12-31 10:25:48 +0300

    Коммит

--- SystemControleProject/wsgi.py
+++ SystemControleProject/wsgi.py
@@ -1,18 +1,8 @@
 import os
-import sys
-
-# Укажите путь к проекту
-project_path = '/home/Bardak/SystemControleProject'
-if project_path not in sys.path:
-    sys.path.append(project_path)
-
-# Укажите путь к виртуальному окружению
-activate_env = '/home/Bardak/IT/curs/venv/bin/activate'
-exec(open(activate_env).read(), {'__file__': activate_env})
+from django.core.wsgi import get_wsgi_application
 
 # Укажите настройки Django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'SystemControleProject.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SystemControleProject.settings')
 
 # Загрузка WSGI приложения
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
