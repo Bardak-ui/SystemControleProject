a7979fcacdc1a833bbd305cc638379fc4ccd8343
Bardak <iiidaniiliii6183@gmail.com>
2025-01-02 01:17:07 +0300

    2.01.2025

--- SystemControleProject/settings.py
+++ SystemControleProject/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 # Настройка статических файлов
 STATIC_URL = '/static/'  # URL для доступа к статическим файлам
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'scp/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
