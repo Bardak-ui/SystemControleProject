9304a1bc01b764158f9547b18a61df59f73d17db
Bardak <iiidaniiliii6183@gmail.com>
2025-01-06 16:54:18 +0300

    Добавление WebSocket Для онлайн и офлайн пользователя

--- staticfiles/websocket.js
+++ staticfiles/websocket.js
@@ -1,5 +1,5 @@
 // websocket.js
-const socket = new WebSocket("ws://0.0.0.0:8000/ws/online_status/");
+const socket = new WebSocket("ws://scp-p141.onrender.com:8000/ws/online_status/");
 
 socket.onopen = function(event) {
     console.log('WebSocket подключен.');
